bc671a3770ecef70bc06fd5deaa124b0b7401459
julianapeace <chancecordelia@gmail.com>
2017-11-10 20:24:11 -0600

    project revivial

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require ('express')
+var app = express()
+const importEnv = require('import-env')
+const port = process.env.PORT || 8000;
+const body_parser = require('body-parser');
+app.use(body_parser.urlencoded({extended: false}));
+
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.render('index.hbs');
+});
+
+
+app.listen(port, function(){
+  console.log('listening on port ' + port)
+});
