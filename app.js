1f5d2f208f9489ab9e892572647932ebd3fc9a73
julianapeace <chancecordelia@gmail.com>
2017-11-10 20:30:00 -0600

    changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require ('express')
 var app = express()
+var axios = require ('axios')
 const importEnv = require('import-env')
 const port = process.env.PORT || 8000;
 const body_parser = require('body-parser');
@@ -11,7 +12,6 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
   res.render('index.hbs');
 });
-
 
 app.listen(port, function(){
   console.log('listening on port ' + port)
